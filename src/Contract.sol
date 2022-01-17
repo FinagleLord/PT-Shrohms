f5d5af4d1a411cc34202e00c7b09894b2a151538
0xBarista <0xBarista@protonmail.com>
2022-01-16 22:08:31 -0500

    compiles

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,14 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
-// import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+abstract contract ReentrancyGuard {
+    uint256 private locked = 1;
+
+    modifier nonReentrant() {
+        require(locked == 1, "REENTRANCY");
+
+        locked = 2;
+
+        _;
+
+        locked = 1;
+    }
+}
 
 contract Colony {
     // max approve PTShrohms contract to handle funds
@@ -17,7 +27,7 @@ contract Colony {
     }
 }
 
-contract PTShrohms is VRFConsumerBase {
+contract PTShrohms is VRFConsumerBase, ReentrancyGuard {
 
     /* -------------------------------------------------------------------------- */
     /*                                DEPENDENCIES                                */
@@ -41,6 +51,7 @@ contract PTShrohms is VRFConsumerBase {
         bytes32 requestId;                      // chainlink request id
         uint256 randomness;                     // chainlink provided randomness
         uint256 amount;                         // total raffle payout token distrobution
+        uint256 length;                         // amount of winners
         mapping(uint256 => bool) hasClaimed;    // has an acocunt claimed their winnings
     }
 
@@ -60,6 +71,8 @@ contract PTShrohms is VRFConsumerBase {
     /* -------------------------------------------------------------------------- */
     /*                                   STORAGE                                  */
     /* -------------------------------------------------------------------------- */
+
+    uint256 public length;
 
     /// @notice Returns minimum amount of time that must pass before another drawing
     uint256 public minEpochLength;
@@ -119,7 +132,8 @@ contract PTShrohms is VRFConsumerBase {
         uint256 _chainlinkFee,
         bytes32 _chainlinkHash,
         address _vrfCoordinator,
-        address _link
+        address _link,
+        uint256 _length
     ) VRFConsumerBase(_vrfCoordinator, _link) {
         payoutToken = _payoutToken;
         shrohms = _shrohms;
@@ -129,6 +143,7 @@ contract PTShrohms is VRFConsumerBase {
         chainlinkHash = _chainlinkHash;
         // set to now to check against later
         lastWithdrawTime = block.timestamp;
+        length = _length;
     }
 
     /* -------------------------------------------------------------------------- */
@@ -148,7 +163,7 @@ contract PTShrohms is VRFConsumerBase {
         uint256 total;
 
         // fetch sum of _newAllocations
-        for (uint256 i; i < _newAllocations; i++) {
+        for (uint256 i; i < _newAllocations.length; i++) {
             total += _newAllocations[i];
         }
 
@@ -180,8 +195,8 @@ contract PTShrohms is VRFConsumerBase {
     }
 
     /// @notice pull unclaimed winnings from an old colony
-    function refund(uint256 gameId) external onlyManager {
-        payoutToken.safeTransferFrom(drawToColony[drawId], manager, payoutToken.balanceOf(address(this)));
+    function refund(uint256 drawId) external onlyManager {
+        payoutToken.safeTransferFrom(address(drawToColony[drawId]), manager, payoutToken.balanceOf(address(this)));
     }
 
     /* -------------------------------------------------------------------------- */
@@ -211,7 +226,7 @@ contract PTShrohms is VRFConsumerBase {
     function claim(
         uint256[] memory tokenIds,
         uint256 drawId
-    ) external lock returns (uint256 totalWinnings) {
+    ) external nonReentrant returns (uint256 totalWinnings) {
         // fetch raffle storage
         Raffle storage raffle = raffles[drawId];
 
@@ -246,27 +261,17 @@ contract PTShrohms is VRFConsumerBase {
     /* -------------------------------------------------------------------------- */
 
     /// @notice calculates up to 10 winners, from a single chainlink randomness seed
-    function calculateWinners(uint256 randomness, uint256 length) internal pure returns (uint256[10] memory winners) {
-
-        winners[0] = uint256(keccak256(abi.encode(randomness))) % shrohmSupply + 1;
-        if (length == 1) return winners;
-        winners[1] = uint256(keccak256(abi.encode(winners[0]))) % shrohmSupply + 1;
-        if (length == 2) return winners;
-        winners[2] = uint256(keccak256(abi.encode(winners[1]))) % shrohmSupply + 1;
-        if (length == 3) return winners;
-        winners[3] = uint256(keccak256(abi.encode(winners[2]))) % shrohmSupply + 1;
-        if (length == 4) return winners;
-        winners[4] = uint256(keccak256(abi.encode(winners[3]))) % shrohmSupply + 1;
-        if (length == 5) return winners;
-        winners[5] = uint256(keccak256(abi.encode(winners[4]))) % shrohmSupply + 1;
-        if (length == 6) return winners;
-        winners[6] = uint256(keccak256(abi.encode(winners[5]))) % shrohmSupply + 1;
-        if (length == 7) return winners;
-        winners[7] = uint256(keccak256(abi.encode(winners[6]))) % shrohmSupply + 1;
-        if (length == 8) return winners;
-        winners[8] = uint256(keccak256(abi.encode(winners[7]))) % shrohmSupply + 1;
-        if (length == 9) return winners;
-        winners[9] = uint256(keccak256(abi.encode(winners[8]))) % shrohmSupply + 1;
+    function calculateWinners(
+        uint256 randomness, 
+        uint256 length
+    ) internal pure returns (uint256[10] memory winners) {
+        // make sure length doesn't not surpass max winners
+        require(length <= 10, "!LENGTH");
+        // update randomness for each winning slot
+        for (uint256 i; i < length; i++) {
+            winners[i] = uint256(keccak256(abi.encode(randomness))) % shrohmSupply + 1;
+            randomness = uint256(keccak256(abi.encode(randomness))) % shrohmSupply + 1;
+        }
     }
 
     /// @notice returns whether a token won, as well as their winning share
@@ -274,10 +279,8 @@ contract PTShrohms is VRFConsumerBase {
         uint256 tokenId,
         uint256 drawId
     ) internal view returns (bool winner, uint256 amount) {
-        // Fetch raffle from memory
-        Raffle memory raffle = raffles[drawId];
         // Determine winners from chainlink seed
-        uint256[10] memory winners = calculateWinners(raffle.randomness, raffle.length);
+        uint256[10] memory winners = calculateWinners(raffles[drawId].randomness, raffles[drawId].length);
         // return if "tokenId" is a winner
         for (uint256 i; i < winners.length; i++) {
             if (winners[i] == tokenId) return (true, raffleAllocations[i]);
--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -2,9 +2,68 @@
 pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
+import "./utils/VM.sol";
+import "../Contract.sol";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SampleERC20 is ERC20("SS","SS") {
+
+}
+contract SampleERC721 is ERC721("77","77") {
+
+}
+
+// mainet info
+// LINK Token	0x514910771AF9Ca656af840dff83E8264EcF986CA
+// VRF Coordinator	0xf0d54349aDdcf704F77AE15b96510dEA15cb7952
+// Key Hash	0xAA77729D3466CA35AE8D28B3BBAC7CC36A5031EFDC430821C02BC31A238AF445
+// Fee	2 LINK 
 
 contract ContractTest is DSTest {
-    function setUp() public {}
+
+    Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    PTShrohms pts;
+
+    // function test_me_hard() public {
+    //     vm.startPrank(address(0))
+    //     pts.callMethod(5);
+    //     vm.stopPrank();
+
+    //     vm.warp();
+
+    //     vm.roll();
+
+    //     vm.expectRevert("!AMOUNT");
+    //     pts.callMethod(5);
+    // }
+
+    function setUp() public {
+
+        SampleERC20 serc20 = new SampleERC20();
+        SampleERC721 serc721 = new SampleERC721();
+
+        IERC20 _payoutToken = IERC20(address(serc20));
+        IERC721 _shrohms = IERC721(address(serc721));
+        uint256 _minEpochLength = 0;
+        uint256 _chainlinkFee = 2 ether;
+        bytes32 _chainlinkHash = 0xAA77729D3466CA35AE8D28B3BBAC7CC36A5031EFDC430821C02BC31A238AF445;
+        address _vrfCoordinator = 0xf0d54349aDdcf704F77AE15b96510dEA15cb7952;
+        address _link = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
+        uint256 _length = 0;
+
+        pts = new PTShrohms(
+            _payoutToken,
+            _shrohms,
+            _minEpochLength,
+            _chainlinkFee,
+            _chainlinkHash,
+            _vrfCoordinator,
+            _link,
+            _length
+        );
+    }
 
     function testExample() public {
         assertTrue(true);
